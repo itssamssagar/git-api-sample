49c811d9e04ed651068939b991d3a990d85261fc
itssamssagar <106534783+itssamssagar@users.noreply.github.com>
2022-06-08 13:53:33 +0530

    Commit to main branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { departmentsModule } from "./modules/departments.module";
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }), MongooseModule.forRoot(process.env.MONGODB_URL, {
     autoCreate: true,
-  })],
+  }),
+        departmentsModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
